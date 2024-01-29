528f17375814ecd7ad3035bc971cbf9525a97ed3
Krissy Clements <144056918+krissyclements@users.noreply.github.com>
2024-01-28 17:58:13 -0700

    Add files via upload

---
+++ homework2/script.js
@@ -0,0 +1,19 @@
+// Console.log statements with comments
+
+// Favorite websites
+console.log("My Favorite Websites");
+console.log("New York Times: https://www.nytimes.com/");
+console.log("Coolors: https://coolors.co/");
+console.log("Netflix: https://www.netflix.com/browse");
+
+// Favorite games
+console.log("\nMy Favorite Games");
+console.log("Mario Kart Double Dash");
+console.log("Phase 10");
+console.log("Wii Sports");
+
+// Favorite artists
+console.log("\nMy Favorite Artists");
+console.log("Claude Monet: Founder of Impressionist Painting"); 
+console.log("Taylor Swift: Lyrical genius with long lasting success and impact, especially for girls/women");
+console.log("Greta Gerwig: Director of influential films, such as Barbie");
